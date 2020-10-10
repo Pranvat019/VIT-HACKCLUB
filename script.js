e8bdb7b96e688f032cc7e3b31088fc72a673f257
pranvat019 <pranvat.singh2020@gmail.com>
2020-10-10 16:22:28 +0530

    basic version of swapping different roles done

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//Gamer, Student, VITian
+// Wait till the DOM is reddy.
+$(function () {
+  var roles = ["Student", "VITian","Gamer"];
+  var count=0;
+  setInterval(() => {
+      $("#intro .content h2 span").text(roles[count++ % roles.length])
+       }, 1000);
+});
