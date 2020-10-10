ca5f0c5ffd5b7bfc7691f9758f4fb964f78acb0f
pranvat019 <pranvat.singh2020@gmail.com>
2020-10-10 16:29:47 +0530

    rolled rotation added animation

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ $(function () {
   var roles = ["Student", "VITian","Gamer"];
   var count=0;
   setInterval(() => {
-      $("#intro .content h2 span").text(roles[count++ % roles.length])
-       }, 1000);
+      $("#intro .content h2 span").fadeOut("fast", function() {
+          $(this).text(roles[count++ % roles.length]);
+          $(this).fadeIn("fast");
+      });
+  }, 1000);
 });
